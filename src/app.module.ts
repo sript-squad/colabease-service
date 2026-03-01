abb416982907b6e01573219900c736fe2c2f6ad2
LasiSipsara <arachch-se20056@stu.kln.ac.lk>
2026-03-01 18:48:09 +0530

    initial project structure

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProjectManagementModule } from './project-management/project-management.module';
+import { TaskManagementModule } from './task-management/task-management.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { AiModule } from './ai/ai.module';
+import { ChatModule } from './chat/chat.module';
+
+@Module({
+  imports: [ProjectManagementModule, TaskManagementModule, NotificationsModule, AiModule, ChatModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
